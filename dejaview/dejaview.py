f3f21870118ba54c287a9fe5c4f6c2088c5dca47
Mark Drago <markdrago@gmail.com>
2012-12-10 08:13:02 -0500

    introduce setup mode to gather all required config up front

--- dejaview/dejaview.py
+++ dejaview/dejaview.py
@@ -1,26 +1,30 @@
 import readline
 import sys
-import getpass
 
 import crucible
 import config
+import config_ui
 
 def main():
-    conf = config.Config()
-    conf.load_from_file()
+    if sys.argv[1] == 'setup':
+        return setup()
 
-    crucible_url = get_crucible_url(conf)
+    conf = load_config()
+    if conf is None:
+        return 1
+
+    crucible_url = conf.get_value('crucible', 'url')
     crucible_conn = crucible.Crucible(crucible_url)
-    username = get_username(conf)
-    auth_token = get_auth_token(conf, crucible_conn, username)
-    project_key = get_project_key(conf)
+    username = conf.get_value('crucible', 'username')
+    auth_token = conf.get_value('crucible', 'token')
+    project_key = conf.get_value('crucible', 'project_key')
 
-    review_id = create_review(crucible_conn, username, auth_token, project_key)
-    print crucible_url + "/cru/" + review_id
-
-def create_review(crucible, username, auth_token, project_key):
     patch = sys.stdin.read()
 
+    review_id = create_review(crucible_conn, username, auth_token, project_key, patch)
+    print crucible_url + "/cru/" + review_id
+
+def create_review(crucible, username, auth_token, project_key, patch):
     parameters = {
         'allow_reviewers_to_join': True,
         'author': username,
@@ -35,55 +39,32 @@ def create_review(crucible, username, auth_token, project_key):
     #print resp.text
     return resp.json['permaId']['id']
 
-def get_crucible_url(conf):
-    url = conf.get_value('crucible', 'url')
-    if url is None:
-        url = acquire_crucible_url()
-        conf.set_value('crucible', 'url', url)
-    return url
+def setup():
+    conf = config.Config()
+    conf.load_from_file()
+    config_ui.setup_config_except_auth_token(conf)
 
-def acquire_crucible_url():
-    prompt = "Please enter the URL for crucible (probably something like http://hostname.com/fisheye): "
-    return raw_input(prompt)
+    crucible_url = conf.get_value('crucible', 'url')
+    crucible_conn = crucible.Crucible(crucible_url)
 
-def get_project_key(conf):
-    project_key = conf.get_value('crucible', 'project_key')
-    if project_key is None:
-        project_key = prompt_for_project_key()
-        conf.set_value('crucible', 'project_key', project_key)
-    return project_key
+    config_ui.setup_auth_token(conf, crucible_conn)
 
-def prompt_for_project_key():
-    prompt = "Please enter your crucible project key (probably something like CR): "
-    return raw_input(prompt)
+    return 0
 
-def get_username(conf):
-    username = conf.get_value('crucible', 'username')
-    if username is None:
-        username = prompt_for_username()
-        conf.set_value('crucible', 'username', username)
-    return username
+def load_config():
+    """load config, check for required fields, print error if any are missing"""
+    conf = config.Config()
+    conf.load_from_file()
+    if not has_all_required_fields(conf):
+        print "Your configuration is incomplete, please run 'dejaview setup' to get that fixed up"
+        return None
+    return conf
 
-def prompt_for_username():
-    prompt = "Please enter your crucible username: "
-    return raw_input(prompt)
-
-def get_auth_token(conf, crucible, username):
-    token = conf.get_value('crucible', 'token')
-    if token is None:
-        token = acquire_auth_token(crucible, username)
-        conf.set_value('crucible', 'token', token)
-    return token
-
-def acquire_auth_token(crucible, username):
-    password = prompt_for_password()
-    response = crucible.get_auth_token(username, password)
-    return response.json['token']
-
-def prompt_for_password():
-    print "In order to get an auth token from crucible, you must enter your password once."
-    password = getpass.getpass()
-    return password
+def has_all_required_fields(conf):
+    for field in ('url', 'username', 'token', 'project_key'):
+        if conf.get_value('crucible', field) is None:
+            return False
+    return True
 
 if __name__ == '__main__':
     sys.exit(main())
---
+++ test/test_dejaview.py
@@ -0,0 +1,48 @@
+import unittest
+
+from dejaview import dejaview
+
+class TestDejaView(unittest.TestCase):
+    def setUp(self):
+        self.c = MockConfig()
+        self.c.set_value('crucible', 'url', 'http://host/fisheye')
+        self.c.set_value('crucible', 'username', 'mdrago')
+        self.c.set_value('crucible', 'token', 'blahblah')
+        self.c.set_value('crucible', 'project_key', 'CR')
+
+    def test_required_fields_all_present(self):
+        self.assertTrue(dejaview.has_all_required_fields(self.c))
+
+    def test_required_fields_url_missing(self):
+        self.c.remove_value('crucible', 'url')
+        self.assertFalse(dejaview.has_all_required_fields(self.c))
+
+    def test_required_fields_username_missing(self):
+        self.c.remove_value('crucible', 'username')
+        self.assertFalse(dejaview.has_all_required_fields(self.c))
+
+    def test_required_fields_token_missing(self):
+        self.c.remove_value('crucible', 'token')
+        self.assertFalse(dejaview.has_all_required_fields(self.c))
+
+    def test_required_fields_project_key_missing(self):
+        self.c.remove_value('crucible', 'project_key')
+        self.assertFalse(dejaview.has_all_required_fields(self.c))
+
+class MockConfig(object):
+    def __init__(self):
+        self.sections = {}
+
+    def set_value(self, section, name, value):
+        if section not in self.sections:
+            self.sections[section] = {}
+        self.sections[section][name] = value
+
+    def remove_value(self, section, name):
+        if section in self.sections and name in self.sections[section]:
+            del self.sections[section][name]
+
+    def get_value(self, section, name):
+        if section not in self.sections or name not in self.sections[section]:
+            return None
+        return self.sections[section][name]
