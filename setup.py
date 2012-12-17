79f2993b93740d43879a370b9b7d219416f25536
Mark Drago <markdrago@gmail.com>
2012-12-17 12:44:52 -0500

    require unittest2 to run unit tests

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         ]
     },
     install_requires=['requests', 'argparse'],
-    tests_require=['mock'],
+    tests_require=['unittest2', 'mock'],
     test_suite='unittest2.collector',
     classifiers=[
         'Programming Language :: Python',
